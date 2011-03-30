de4e7bb3495f8a764b3234248e88f310da3730a2
Braden Schaeffer <braden.schaeffer@gmail.com>
2011-03-30 15:41:36 -0500

    initial Lang class and tag replacement

--- themer/parser/lang.php
+++ themer/parser/lang.php
@@ -16,7 +16,11 @@
  */
 
 namespace Themer\Parser;
-use Themer\Parser as Parser;
+
+use Themer\Data;
+use Themer\Parser\Variable;
+
+require_once 'SymfonyComponents/YAML/sfYaml.php';
 
 /**
  * Themer Language Class 
@@ -29,18 +33,41 @@ use Themer\Parser as Parser;
  */
 class Lang {
   
-  const MATCHER = '/{lang:([A-Z][A-Za-z0-9-\s]*)}/';
+  private static $_lang = 'en';
   
   /**
-   * Parses Tumblr's 
+   * Parses Tumblr's lang:Item tags
    * 
    * @static
    * @access  public
-   * @return  void
+   * @param   string  the theme text
+   * @return  string  the parsed theme text
    */
-  public static function parse()
+  public static function render($theme)
   {
+    $lang = self::_load_lang();
     
+    foreach($lang as $k => $v)
+    {
+      $tag = 'lang:'.$k;
+      $theme = Variable::render($theme, $tag, $v, FALSE);
+    }
+    
+    return $theme;
+  }
+  
+  /**
+   * Loads a language file
+   * 
+   * @static
+   * @access  private
+   * @return  array   the language data
+   */
+  private static function _load_lang()
+  {
+    $path = THEMER_BASEPATH.'themer/locales/'.static::$_lang.YML;
+    $lang = \sfYaml::load($path);
+    return $lang;
   }
 }
 
