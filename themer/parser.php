42219562f9d31184726ceacfd2d1d1760b93c147
Braden Schaeffer <hello@manasto.info>
2011-04-06 23:29:36 -0500

    enable parsing of HasPages blocks

--- themer/parser.php
+++ themer/parser.php
@@ -71,12 +71,65 @@ class Parser {
     # The order in which these are rendered doesn't really matter.
     $theme = Paginate::render($theme);
     $theme = Pages::render($theme);
+    $theme = self::render_pages($theme);
     
     # Posts should be rendered second to last. If it weren't for some tag name
     # clashes (ie. {Title} for both blog and text post title), they would be
     # rendered dead last. Second to last at least allows all other blocks, and 
     # theme tags to be rendered into/alongside with each post (if desired).
     $theme = Posts::render($theme, static::$post_data);
+    
+    return $theme;
+  }
+  
+  /**
+   * Renders Tumblr's {block:HasPages} blocks
+   * 
+   * @access  public
+   * @param   string  the theme contents to parse
+   * @return  string  the parsed theme contents
+   */
+  public static function render_pages($theme)
+  { 
+    $data = isset(static::$data['Pages']) ? static::$data['Pages'] : array();
+    
+    if(empty($data))
+    {
+      return Block::remove($theme, 'HasPages');
+    }
+    
+    foreach(Block::find($theme, 'HasPages') as $has_pages)
+    {
+      // Set a temporary, rendered HasPages block
+      $tmp = Block::render($has_pages, 'HasPages');
+  
+      // Find all the {block:Pages} blocks in the current block
+      foreach(Block::find($tmp, 'Pages') as $page)
+      {
+        // Clear what has already been rendered for each page
+        $rendered = '';
+        
+        foreach($data as $page_data)
+        {  
+          // Render {block:Pages} for each page
+          $block = Block::render($page, 'Pages');
+          
+          foreach($page_data as $tag => $value)
+          {
+            $block = Variable::render($block, $tag, $value);
+          }
+          
+          // Appened the rendered content
+          $rendered .= $block;
+        }
+        
+        // Replace the original {block:HasPages} block with rendered content
+        $tmp = str_replace($page, $rendered, $tmp);
+      }
+      
+      // Add it to the theme
+      $theme = str_replace($has_pages, $tmp, $theme);
+    }
     
     return $theme;
   }
